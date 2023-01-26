0587b4b7afbbb7f6f2ceef22ee81bbc6195339e5
AntonioHern <antoniohernba@gmail.com>
2023-01-27 00:40:47 +0100

    Initial commit

---
+++ back/index.js
@@ -0,0 +1,41 @@
+//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
+const {Configuration, OpenAIApi} = require("openai");
+require('dotenv').config()
+const configuration = new Configuration({
+    organization: "org-eKhMRA48hf6Roe77t2BNqRLm",
+    apiKey: process.env.OPENAI_API_KEY
+});
+const openai = new OpenAIApi(configuration);
+
+
+
+// crate a simple express server api that will call the function above
+const express = require('express')
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express()
+app.use(bodyParser.json());
+//importante para que funcione el post
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+const port = 3000
+
+app.post('/',  async (req, res) => {
+    //const message = req.body
+
+    // console.log(message)
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: req.body.text,
+        max_tokens: 100,
+        temperature: 0.5,
+    })
+   // console.log(response.data.choices[0].text)
+    res.json({
+        data: response.data.choices[0].text
+    })
+});
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
+
